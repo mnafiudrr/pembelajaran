0fc9023f276cdc97c92bcfaf9d2e9423e2f5499b
riobagus <rbagus02@gmail.com>
2022-09-27 21:39:42 +0700

    nambahin logo di title bar

--- resources/views/partials/master.blade.php
+++ resources/views/partials/master.blade.php
@@ -9,6 +9,8 @@
 
         <title>Eksporasi</title>
 
+        <link rel="icon" href="{{ asset('keperluan/logo.png') }}" type="image/x-icon">
+
         <!-- Custom fonts for this template-->
         <link href="{{asset('template-admin/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
