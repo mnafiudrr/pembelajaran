8e946418c9f47202db2fd9b5b781b79feb1d86e0
Nafiu <nafiu@pop-os.localdomain>
2022-11-12 11:16:43 +0700

    fix: api get pengaturan

--- app/Http/Controllers/Api/PengaturanController.php
+++ app/Http/Controllers/Api/PengaturanController.php
@@ -10,12 +10,22 @@ use Illuminate\Http\Request;
 class PengaturanController extends Controller
 {
     public function index(){
-        $data = Pengaturan::all();
+        // $data = Pengaturan::all();
 
-        if($data){
-            return ApiFormatter::createApi(200, 'Success', $data);
-        }else{
-            return ApiFormatter::createApi(400, 'Failed');
-        }
+        // if($data){
+        //     return ApiFormatter::createApi(200, 'Success', $data);
+        // }else{
+        //     return ApiFormatter::createApi(400, 'Failed');
+        // }
+        $data = Pengaturan::where('status', 1)->select('logo', 'tagline', 'header', 'link')->first();
+
+        return response()->json([
+            'data' => [
+                'logo' => url($data->logo),
+                'tagline' => $data->tagline,
+                'header' => url($data->header),
+                'video' => $data->link
+            ]
+        ], 200);
     }
 }
