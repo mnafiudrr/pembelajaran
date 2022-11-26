6390b97773d5632d0f26ad1e332c94917f2930e5
Nafiu <nafiu@pop-os.localdomain>
2022-11-26 13:10:31 +0700

    fix: berita api

--- app/Http/Controllers/Api/BeritaController.php
+++ app/Http/Controllers/Api/BeritaController.php
@@ -10,12 +10,66 @@ use Illuminate\Http\Request;
 class BeritaController extends Controller
 {
     public function index(){
-        $data = Berita::all();
+        $berita = Berita::select('id', 'judul', 'photo1')->get();
 
-        if($data){
-            return ApiFormatter::createApi(200, 'Success', $data);
-        }else{
-            return ApiFormatter::createApi(400, 'Failed');
-        }
+        $data = $berita->transform(
+            function ($item) {
+                return [
+                    'id' => $item->id,
+                    'title' => $item->judul,
+                    'icon' => url($item->photo1)
+                ];
+            }
+        );
+        
+        return response()->json([
+            "data" => $data,
+            "meta" => [
+                "status" => "success"
+            ]
+        ]);
+    }
+
+    public function show($id)
+    {
+        $berita = Berita::find($id);
+
+        $data = [
+            [
+                'content_type_id' => 2,
+                'value' => $berita->photo1?url($berita->photo1):null,
+            ],
+            [
+                'content_type_id' => 2,
+                'value' => $berita->photo2?url($berita->photo2):null,
+            ],
+            [
+                'content_type_id' => 2,
+                'value' => $berita->photo3?url($berita->photo3):null,
+            ],
+            [
+                'content_type_id' => 1,
+                'value' => $berita->paragraf1,
+            ],
+            [
+                'content_type_id' => 1,
+                'value' => $berita->paragraf2,
+            ],
+            [
+                'content_type_id' => 1,
+                'value' => $berita->paragraf3,
+            ],
+            [
+                'content_type_id' => 1,
+                'value' => $berita->paragraf4,
+            ],
+        ];
+        
+        return response()->json([
+            "data" => $data,
+            "meta" => [
+                "message" => "success"
+            ]
+        ]);
     }
 }
